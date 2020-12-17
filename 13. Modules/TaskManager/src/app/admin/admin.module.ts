21e3d4aa828b5a03928c7b6ba311591c07f85663
Harsha Vardhan <harshaglobal@gmail.com>
2020-12-17 19:17:14 +0530

    Angular

---
+++ 13. Modules/TaskManager/src/app/admin/admin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [DashboardComponent, MyProfileComponent, AboutComponent],
+  imports: [CommonModule],
+  exports: [DashboardComponent, MyProfileComponent, AboutComponent],
+  providers: [],
+})
+export class AdminModule {}
---
+++ 13. Modules/TaskManager/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AboutComponent } from './admin/about/about.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
