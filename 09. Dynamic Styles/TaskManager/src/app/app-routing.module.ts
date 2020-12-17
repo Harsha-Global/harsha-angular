f4c9d8ee08acd3978aef7b10847a4494ebcf4e17
Harsha Vardhan <harshaglobal@gmail.com>
2020-12-17 18:18:10 +0530

    Angular

---
+++ 09. Dynamic Styles/TaskManager/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "about", component: AboutComponent },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule
+{
+}
