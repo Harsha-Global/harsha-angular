fe35728719290feef756582e334f48779d360250
Harsha-Global <Harsha-Global@users.noreply.github.com>
2021-02-20 16:11:56 +0530

    Update dashboard.component.ts

--- 06. Dashboard/TaskManager/src/app/dashboard/dashboard.component.ts
+++ 06. Dashboard/TaskManager/src/app/dashboard/dashboard.component.ts
@@ -5,24 +5,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
-  Designation: string;
-  Username: string;
-  NoOfTeamMembers: number;
-  TotalCostOfAllProjects: number;
-  PendingTasks: number;
-  UpComingProjects: number;
-  ProjectCost: number;
-  CurrentExpenditure: number;
-  AvailableFunds: number;
+export class DashboardComponent implements OnInit
+{
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number = 0;
+  UpComingProjects: number = 0;
+  ProjectCost: number = 0;
+  CurrentExpenditure: number = 0;
+  AvailableFunds: number = 0;
 
-  Clients: string[];
-  Projects: string[];
+  Clients: string[] = [];
+  Projects: string[] = [];
   Years: number[] = [];
-  TeamMembersSummary = [];
-  TeamMembers = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
 
-  ngOnInit() {
+  ngOnInit()
+  {
     this.Designation = 'Team Leader';
     this.Username = 'Scott Smith';
     this.NoOfTeamMembers = 67;
