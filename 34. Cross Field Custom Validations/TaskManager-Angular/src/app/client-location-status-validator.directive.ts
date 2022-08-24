431cff7f7260e7a83e7605268bdd23e129cb8bb2
Harsha-Global <53711684+Harsha-Global@users.noreply.github.com>
2022-08-24 14:36:07 +0530

    Project Update

--- 34. Cross Field Custom Validations/TaskManager-Angular/src/app/client-location-status-validator.directive.ts
+++ 34. Cross Field Custom Validations/TaskManager-Angular/src/app/client-location-status-validator.directive.ts
@@ -3,29 +3,23 @@ import { NG_VALIDATORS, Validator, AbstractControl, ValidationErrors } from '@an
 
 @Directive({
   selector: '[appClientLocationStatusValidator]',
-  providers: [ { provide: NG_VALIDATORS, useExisting: ClientLocationStatusValidatorDirective, multi: true }]
+  providers: [{ provide: NG_VALIDATORS, useExisting: ClientLocationStatusValidatorDirective, multi: true }]
 })
-export class ClientLocationStatusValidatorDirective implements Validator
-{
-  constructor()
-  {
+export class ClientLocationStatusValidatorDirective implements Validator {
+  constructor() {
   }
 
-  validate(control: AbstractControl) : ValidationErrors | null
-  {
+  validate(control: AbstractControl): ValidationErrors | null {
     let isValid = true;
-    if (control.value.ClientLocation == 6 && control.value.Status == "Support")
-    {
+    if (control.value.ClientLocation == 1 && control.value.Status == "Support") {
       isValid = false; //indicates invalid
     }
 
-    if (isValid == true)
-    {
+    if (isValid == true) {
       return null; //valid
     }
-    else
-    {
-      return { clientLocationStatus: { valid: false }}; //invalid
+    else {
+      return { clientLocationStatus: { valid: false } }; //invalid
     }
   }
 
