93df1c44fdcc40f1d8488432abd2783bd666b047
Harsha Vardhan <harshaglobal@gmail.com>
2026-04-11 14:07:14 +0530

    Angular Latest Version

--- Old/15. Dynamic Components/01. Preparing for Dynamic Components/TaskManager-Angular/src/app/models/client-location.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ClientLocation } from './client-location';
-
-describe('ClientLocation', () => {
-  it('should create an instance', () => {
-    expect(new ClientLocation()).toBeTruthy();
-  });
-});
