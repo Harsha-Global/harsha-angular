fc9ca91e0dabc7a1f76ce73c86e77beae819395d
Harsha Vardhan <harshaglobal@gmail.com>
2026-05-22 17:25:05 +0530

    Angular Latest Version

--- Old/12. Advanced Routing/07. CanDeactivate Guard/TaskManager-Angular/src/app/project-idunique-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectIDUniqueValidatorDirective } from './project-idunique-validator.directive';
-
-describe('ProjectIDUniqueValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ProjectIDUniqueValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
