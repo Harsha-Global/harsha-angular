4a62b436f8167b1074b8a84b035bd711193703c7
Harsha Vardhan <harshaglobal@gmail.com>
2026-09-02 13:33:13 +0530

    Angular Update

--- Old/11. Directives/02. Input Properties/TaskManager-Angular/src/app/sign-up-view-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SignUpViewModel } from './sign-up-view-model';
-
-describe('SignUpViewModel', () => {
-  it('should create an instance', () => {
-    expect(new SignUpViewModel()).toBeTruthy();
-  });
-});
--- Old/11. Directives/05. Renderer2/TaskManager-Angular/src/app/paging.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PagingPipe } from './paging.pipe';
-
-describe('PagingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PagingPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
